4e35b55f06b062c0c802d45c0c67a8cab353c129
Shawshank08 <shashank08020@gmail.com>
2025-12-18 21:59:28 +0530

    chore:DB verification

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-
+const connectDB = require('./config/db'); 
 const app = express();
 
-// test route
+app.use(express.json());
+connectDB();
 app.get('/', (req, res) => {
-  res.send('Server is running in dev mode');
+  res.send('Basckend running');
 });
 
 const PORT = process.env.PORT || 5000;
